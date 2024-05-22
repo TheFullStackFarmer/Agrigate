2be7637f14b256e26c47a4a6e355839b21083771
Kevin Williams <kevin.williams@thefullstackfarmer.com>
2024-05-22 18:37:02 -0500

    MQTT code cleanup

--- src/Agrigate.IoT/Actors/Devices/Device.cs
+++ src/Agrigate.IoT/Actors/Devices/Device.cs
@@ -1,8 +1,6 @@
 using Agrigate.Core.Configuration;
-using Akka.Actor;
 using Akka.Event;
 using Microsoft.Extensions.Options;
-using MQTTnet;
 using MQTTnet.Client;
 
 namespace Agrigate.IoT.Actors.Devices;
@@ -10,121 +8,30 @@ namespace Agrigate.IoT.Actors.Devices;
 /// <summary>
 /// An actor representing a single device connected to the Agrigate platform
 /// </summary>
-public class Device : ReceiveActor
+public class Device : MQTTActor
 {
-    private readonly ILoggingAdapter _log;
-    private readonly ServiceConfiguration _configuration;
-
     private readonly string _deviceId;
-    private readonly MqttFactory _mqttFactory;
 
-    private IMqttClient? _mqttClient;
-    private MqttClientOptions? _options;
-
-    public Device(IOptions<ServiceConfiguration> options, string deviceId, MqttFactory mqttFactory)
+    public Device(IOptions<ServiceConfiguration> options, string deviceId) : base(options)
     {
-        _log = Logging.GetLogger(Context) ?? throw new ApplicationException("Unable to retrieve logger");
-        _configuration = options.Value ?? throw new ArgumentNullException(nameof(options));
         _deviceId = deviceId ?? throw new ArgumentNullException(nameof(deviceId));
-        _mqttFactory = mqttFactory ?? throw new ArgumentNullException(nameof(mqttFactory));
     }
 
     protected override void PreStart()
     {
-        _log.Info($"Device {_deviceId} initializing...");
+        Log.Info($"Device {_deviceId} initializing...");
 
-        ConnectToBroker();
-        SubscribeToDeviceEvents();
+        ConnectToBroker($"agrigate-{_deviceId}", HandleDeviceEvent);
+        SubscribeToTopic($"devices/{_deviceId}");
         // TODO: Query & Create device in database
 
-        _log.Info($"Device {_deviceId} ready.");
+        Log.Info($"Device {_deviceId} ready.");
     }
 
     protected override void PostStop()
     {
-        DisconnectFromBroker();
-        DisposeBrokerConnection();
-        _log.Info($"Device {_deviceId} terminated.");
-    }
-
-    private void ConnectToBroker()
-    {
-        try 
-        {
-            _options = new MqttClientOptionsBuilder()
-                .WithClientId($"agrigate-{_deviceId}")
-                .WithTcpServer(_configuration.MQTTHostname)
-                .Build();
-            _mqttClient = _mqttFactory.CreateMqttClient();
-
-            _mqttClient.DisconnectedAsync += Reconnect;
-            _mqttClient.ApplicationMessageReceivedAsync += HandleDeviceEvent;
-
-            _mqttClient.ConnectAsync(_options, CancellationToken.None).Wait();
-        }
-        catch (Exception ex)
-        {
-            _log.Error($"Unable to connect to broker: {ex.Message}");
-            throw;
-        }
-    }
-
-    private void DisconnectFromBroker()
-    {
-        try 
-        {
-            if (_mqttClient == null)
-                throw new ApplicationException("MQTT Client was not initialized");
-
-            var disconnectOptions = new MqttClientDisconnectOptionsBuilder()
-                .WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection)
-                .Build();
-
-            _mqttClient.DisconnectAsync(disconnectOptions, CancellationToken.None).Wait();
-        }
-        catch (Exception ex)
-        {
-            _log.Error($"Unable to disconnect from broker: {ex.Message}");
-        }
-    }
-
-    private void DisposeBrokerConnection()
-    {
-        _mqttClient?.Dispose();
-    }
-
-    private void SubscribeToDeviceEvents()
-    {
-        if (_mqttClient == null)
-        {
-            var message = "MQTT Client failed to initialize";
-            _log.Error(message);
-            throw new ApplicationException(message);
-        }
-
-        try 
-        {
-            var subscriptionOptions = _mqttFactory
-                .CreateSubscribeOptionsBuilder()
-                .WithTopicFilter(f => { f.WithTopic($"devices/{_deviceId}"); })
-                .Build();
-
-            _mqttClient.SubscribeAsync(subscriptionOptions, CancellationToken.None).Wait();
-        }
-        catch (Exception ex)
-        {
-            _log.Error($"Unable to subscribe to device events: {ex.Message}");
-            throw;
-        }
-    }
-
-    private async Task Reconnect(MqttClientDisconnectedEventArgs e)
-    {
-        if (_mqttClient == null || _options == null)
-            return;
-
-        if (e.ClientWasConnected)
-            await _mqttClient.ConnectAsync(_options);
+        base.PostStop();
+        Log.Info($"Device {_deviceId} terminated.");
     }
 
     private Task HandleDeviceEvent(MqttApplicationMessageReceivedEventArgs e)
@@ -133,14 +40,14 @@ public class Device : ReceiveActor
         {
             // TODO: Handle events - only telemetry at the moment
             var message = System.Text.Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment);
-            _log.Info(message);
+            Log.Info(message);
 
             // var timestamp = DateTimeOffset.FromUnixTimeMilliseconds(connectionEvent?.Timestamp ?? 0);
             // DateTime.TryParse(connectionEvent.ConnectedAt, out var time))
         }
         catch (Exception ex)
         {
-            _log.Error($"Unable to process device event: {ex.Message}");
+            Log.Error($"Unable to process device event: {ex.Message}");
         }
 
         return Task.CompletedTask;
--- src/Agrigate.IoT/Actors/Devices/DeviceManager.cs
+++ src/Agrigate.IoT/Actors/Devices/DeviceManager.cs
@@ -6,7 +6,6 @@ using Akka.DependencyInjection;
 using Akka.Event;
 using Akka.Util.Internal;
 using Microsoft.Extensions.Options;
-using MQTTnet;
 using MQTTnet.Client;
 using Newtonsoft.Json;
 
@@ -15,120 +14,21 @@ namespace Agrigate.IoT.Actors.Devices;
 /// <summary>
 /// The actor responsible for managing devices connected to Agrigate
 /// </summary>
-public class DeviceManager : ReceiveActor
+public class DeviceManager : MQTTActor
 {
-    private readonly ILoggingAdapter _log;
-    private readonly ServiceConfiguration _configuration;
-
-    private MqttFactory? _mqttFactory;
-    private IMqttClient? _mqttClient;
-    private MqttClientOptions? _options;
-
     private readonly IActorContext _context;
     private ConcurrentDictionary<string, IActorRef> _deviceActors;
 
-    public DeviceManager(IOptions<ServiceConfiguration> options)
+    public DeviceManager(IOptions<ServiceConfiguration> options) : base(options)
     {
-        _log = Logging.GetLogger(Context) ?? throw new ApplicationException("Unable to retrieve logger");
-        _configuration = options.Value ?? throw new ArgumentNullException(nameof(options));
-
         _deviceActors = new ConcurrentDictionary<string, IActorRef>();
         _context = Context;
     }
 
     protected override void PreStart()
     {
-        ConnectToBroker();
-        SubscribeToConnectionEvents();
-    }
-
-    protected override void PostStop()
-    {
-        DisconnectFromBroker();
-        DisposeBrokerConnection();
-    }
-
-    private void ConnectToBroker()
-    {
-        try 
-        {
-            _mqttFactory = new MqttFactory();
-            _options = new MqttClientOptionsBuilder()
-                .WithClientId("agrigate-device-manager")
-                .WithTcpServer(_configuration.MQTTHostname)
-                .Build();
-            _mqttClient = _mqttFactory.CreateMqttClient();
-
-            _mqttClient.DisconnectedAsync += Reconnect;
-            _mqttClient.ApplicationMessageReceivedAsync += HandleConnectionEvent;
-
-            _mqttClient.ConnectAsync(_options, CancellationToken.None).Wait();
-        }
-        catch (Exception ex)
-        {
-            _log.Error($"Unable to connect to broker: {ex.Message}");
-            throw;
-        }
-    }
-
-    private void SubscribeToConnectionEvents()
-    {
-        if (_mqttFactory == null || _mqttClient == null)
-        {
-            var message = "MQTT Factory or Client failed to initialize";
-            _log.Error(message);
-            throw new ApplicationException(message);
-        }
-
-        try
-        {
-            var subscriptionOptions = _mqttFactory
-                .CreateSubscribeOptionsBuilder()
-                .WithTopicFilter(f => { f.WithTopic(@"$SYS/brokers/+/clients/#"); })
-                .Build();
-
-            _mqttClient.SubscribeAsync(subscriptionOptions, CancellationToken.None).Wait();
-        }
-        catch (Exception ex)
-        {
-            _log.Error($"Unable to subscribe to client events: {ex.Message}");
-            throw;
-        }
-    }
-
-    private void DisconnectFromBroker()
-    {
-        try 
-        {
-            if (_mqttClient == null)
-                throw new ApplicationException("MQTT Client was not initialized");
-                
-            var disconnectOptions = new MqttClientDisconnectOptionsBuilder()
-                .WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection)
-                .Build();
-
-            _mqttClient.DisconnectAsync(disconnectOptions, CancellationToken.None).Wait();
-        }
-        catch (Exception ex)
-        {
-            _log.Error($"Unable to disconnect from broker: {ex.Message}");
-        }
-    }
-
-    private void DisposeBrokerConnection()
-    {
-        _mqttClient?.Dispose();
-    }
-
-    // Event Handlers
-
-    private async Task Reconnect(MqttClientDisconnectedEventArgs e)
-    {
-        if (_mqttClient == null || _options == null)
-            return;
-
-        if (e.ClientWasConnected)
-            await _mqttClient.ConnectAsync(_options);
+        ConnectToBroker("agrigate-device-manager", HandleConnectionEvent);
+        SubscribeToTopic(@"$SYS/brokers/+/clients/#");
     }
 
     private Task HandleConnectionEvent(MqttApplicationMessageReceivedEventArgs e)
@@ -149,7 +49,7 @@ public class DeviceManager : ReceiveActor
             {
                 if (!_deviceActors.TryGetValue(clientId, out var actorRef))
                 {
-                    _log.Warning($"{clientId} is not registered");
+                    Log.Warning($"{clientId} is not registered");
                     return Task.CompletedTask;
                 }
 
@@ -159,16 +59,16 @@ public class DeviceManager : ReceiveActor
 
             else if (connectionEvent.ConnectedAt != null)
             {
-                if (_mqttFactory == null)
+                if (MqttFactory == null)
                     throw new ApplicationException("MQTT Factory is unavailable");
 
                 if (_deviceActors.TryGetValue(clientId, out var actorRef))
                 {
-                    _log.Warning($"{clientId} already registered with {actorRef}");
+                    Log.Warning($"{clientId} already registered with {actorRef}");
                     return Task.CompletedTask;
                 }
                 
-                var deviceProps = DependencyResolver.For(_context.System).Props<Device>(clientId, _mqttFactory);
+                var deviceProps = DependencyResolver.For(_context.System).Props<Device>(clientId);
                 var deviceActor = _context.ActorOf(deviceProps, $"Device-{clientId}");
 
                 _deviceActors.AddOrSet(clientId, deviceActor);
@@ -176,7 +76,7 @@ public class DeviceManager : ReceiveActor
         }
         catch (Exception ex)
         {
-            _log.Error($"Unable to process connection event: {ex.Message}");
+            Log.Error($"Unable to process connection event: {ex.Message}");
         }
 
         return Task.CompletedTask;
